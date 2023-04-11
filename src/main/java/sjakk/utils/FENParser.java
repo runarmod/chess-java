9fd4d5c768c9f233e72dda8e64cc976af5df5f28
Runar Saur Modahl <runarsm@stud.ntnu.no>
2023-04-11 17:30:06 +0200

    documentation fenparser

--- src/main/java/sjakk/utils/FENParser.java
+++ src/main/java/sjakk/utils/FENParser.java
@@ -18,42 +18,67 @@ import sjakk.Player;
 import sjakk.Position;
 import sjakk.pieces.Piece;
 
+/**
+ * A utility class for saving, loading, parsing, generating FEN strings to and
+ * from files and {@link ChessBoard}s.
+ */
 public abstract class FENParser {
+    /**
+     * The file-extention for fen-files.
+     */
     public static final String FEN_EXTENSION = "fen";
+
+    /**
+     * The default FEN string.
+     */
     public static final String DEFAULT_STRING = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
+    /**
+     * Read a FEN string from a file.
+     * 
+     * @param file The file to read from.
+     * @return The FEN string.
+     * @throws FileNotFoundException If the file does not exist.
+     */
     public static String readFENFromFile(File file) throws FileNotFoundException {
         String FENString = "";
-        Scanner scanner = new Scanner(file);
+        final Scanner scanner = new Scanner(file);
         FENString = scanner.nextLine();
         scanner.close();
         return FENString;
     }
 
+    /**
+     * Get a {@link ChessBoard} from a FEN string.
+     * 
+     * @param input The FEN string.
+     * @return The {@link ChessBoard} represented by the FEN string.
+     * @throws IllegalFENException If the FEN string is invalid.
+     */
     public static ChessBoard getBoardFromFEN(String input) throws IllegalFENException {
-        Player white = new Player(PieceColor.WHITE);
-        Player black = new Player(PieceColor.BLACK);
-        ChessBoard board = new ChessBoard(white, black);
-        String[] data = input.split(" ");
+        final Player white = new Player(PieceColor.WHITE);
+        final Player black = new Player(PieceColor.BLACK);
+        final ChessBoard board = new ChessBoard(white, black);
+        final String[] data = input.split(" ");
         if (data.length != 6) {
             throw new IllegalFENException("Invalid FEN string");
         }
-        String[] rows = data[0].split("/");
+        final String[] rows = data[0].split("/");
         if (rows.length != 8) {
             throw new IllegalFENException("Invalid FEN string");
         }
 
         for (int i = 0; i < 8; i++) {
-            String row = rows[i];
+            final String row = rows[i];
 
             int stringIndex = 0;
             int boardIndex = 0;
             while (boardIndex < 8) {
-                char pieceCharacter = row.charAt(stringIndex);
+                final char pieceCharacter = row.charAt(stringIndex);
                 if (Character.isDigit(pieceCharacter)) {
                     boardIndex += Character.getNumericValue(pieceCharacter);
                 } else {
-                    Player player = Character.isUpperCase(pieceCharacter) ? white : black;
+                    final Player player = Character.isUpperCase(pieceCharacter) ? white : black;
                     Piece.placePiece(player, new Position(boardIndex, 7 - i), board, pieceCharacter);
                     boardIndex++;
                 }
@@ -66,7 +91,7 @@ public abstract class FENParser {
 
         // Castling rights
         board.disableCastling();
-        for (char c : data[2].toCharArray()) {
+        for (final char c : data[2].toCharArray()) {
             switch (c) {
                 case 'K':
                     board.setWhiteKingSideCastle(true);
@@ -94,35 +119,33 @@ public abstract class FENParser {
         return board;
     }
 
+    /**
+     * Get a the default {@link ChessBoard} (retreived using the default fen-string)
+     * 
+     * @return The default {@link ChessBoard}.
+     */
     public static ChessBoard getBoardFromDefaultFEN() {
         try {
             return getBoardFromFEN(DEFAULT_STRING);
-        } catch (IllegalFENException e) {
+        } catch (final IllegalFENException e) {
             // Wont happen since the above string is valid
             return null;
         }
     }
 
-    private static String shuffleString(String string) {
-        StringBuilder builder = new StringBuilder(string.length());
-        List<Character> chars = new ArrayList<Character>();
-        for (char c : string.toCharArray()) {
-            chars.add(c);
-        }
-        while (chars.size() != 0) {
-            int randIndex = (int) (Math.random() * chars.size());
-            builder.append(chars.remove(randIndex));
-        }
-        return builder.toString();
-    }
-
+    /**
+     * Returns whether or not a FENString is legal for a Fischer random position.
+     * 
+     * @param FENString The FENString to check.
+     * @return Whether or not the FENString is legal for a Fischer random position.
+     */
     public static boolean legalFischerRandom(String FENString) {
         if (FENString.split("/").length != 8) {
             return false;
         }
 
-        String whiteSide = FENString.split(" ")[0].split("/")[7];
-        String blackSide = FENString.split("/")[0];
+        final String whiteSide = FENString.split(" ")[0].split("/")[7];
+        final String blackSide = FENString.split("/")[0];
 
         if (!legalFirstRowFischerRandom(blackSide))
             return false;
@@ -133,31 +156,11 @@ public abstract class FENParser {
         return blackSide.equals(whiteSide.toLowerCase());
     }
 
-    private static boolean legalFirstRowFischerRandom(String string) {
-        int king = string.indexOf("k");
-        int bishop1 = string.indexOf("b");
-        int bishop2 = string.lastIndexOf("b");
-        int rook1 = string.indexOf("r");
-        int rook2 = string.lastIndexOf("r");
-
-        if ((bishop1 + bishop2) % 2 != 1) {
-            return false;
-        }
-
-        if (!(rook1 < king && king < rook2)) {
-            return false;
-        }
-
-        // Make sure we have the right count of every piece
-        Map<Character, Integer> row = Map.of('r', 2, 'n', 2, 'b', 2, 'q', 1, 'k', 1);
-        for (Character c : string.toCharArray()) {
-            if (string.chars().filter(e -> e == c).count() != row.get(c)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
+    /**
+     * Generates a FEN-string for a Fischer random position.
+     * 
+     * @return the random FEN-string.
+     */
     public static String generateFischerRandomFEN() {
         final String defaultPlacement = "rnbqkbnr";
         String blackSide = defaultPlacement;
@@ -171,22 +174,30 @@ public abstract class FENParser {
                 break;
         }
 
+        // If we didn't find a legal string in 1000 tries, just use the default.
+        // Microscopic chance of this happening.
         if (i >= 1000)
             blackSide = defaultPlacement;
 
-        String whiteSide = blackSide.toUpperCase();
-        String FENString = blackSide + "/pppppppp/8/8/8/8/PPPPPPPP/" + whiteSide + " w - - 0 1";
+        final String whiteSide = blackSide.toUpperCase();
+        final String FENString = blackSide + "/pppppppp/8/8/8/8/PPPPPPPP/" + whiteSide + " w - - 0 1";
         return FENString;
     }
 
+    /**
+     * Generates the FEN-string for a given {@link ChessBoard}.
+     * 
+     * @param board the {@link ChessBoard} to generate the FEN-string for.
+     * @return the generated FENString.
+     */
     public static String generateFEN(ChessBoard board) {
-        StringBuilder FENString = new StringBuilder();
+        final StringBuilder FENString = new StringBuilder();
 
         // First part of the FEN string
         int emptySpaces = 0;
         for (int y = 7; y >= 0; y--) {
             for (int x = 0; x < 8; x++) {
-                Piece piece = board.getPosition(new Position(x, y));
+                final Piece piece = board.getPosition(new Position(x, y));
                 if (piece == null) {
                     emptySpaces++;
                     continue;
@@ -234,16 +245,16 @@ public abstract class FENParser {
     public static void saveToFile(String content, File location) {
         File file = location;
         if (file == null) {
-            File defaultSaveDirectory = new File(
+            final File defaultSaveDirectory = new File(
                     System.getProperty("user.dir") + System.getProperty("file.separator") + "games");
-            JFileChooser chooser = getValidChooser(defaultSaveDirectory);
+            final JFileChooser chooser = getValidChooser(defaultSaveDirectory);
 
-            FileNameExtensionFilter filter = new FileNameExtensionFilter("FEN files", FEN_EXTENSION);
+            final FileNameExtensionFilter filter = new FileNameExtensionFilter("FEN files", FEN_EXTENSION);
             chooser.setFileFilter(filter);
             chooser.setDialogTitle("Save State");
             chooser.setSelectedFile(new File("mygame." + FEN_EXTENSION));
 
-            int returnState = chooser.showSaveDialog(null);
+            final int returnState = chooser.showSaveDialog(null);
             if (returnState == JFileChooser.APPROVE_OPTION) {
                 file = chooser.getSelectedFile();
             }
@@ -254,26 +265,118 @@ public abstract class FENParser {
         }
 
         try {
-            FileOutputStream fw = new FileOutputStream(file);
+            final FileOutputStream fw = new FileOutputStream(file);
             fw.write(content.getBytes());
             fw.close();
-        } catch (Exception e) {
+        } catch (final Exception e) {
         }
     }
 
+    /**
+     * Saves the given FEN string to a file. A file chooser UI will be shown.
+     * 
+     * @param content The FEN string to save
+     * @see #saveToFile(String, File)
+     */
     public static void saveToFile(String content) {
         saveToFile(content, null);
     }
 
+    /**
+     * Get a file from a file chooser UI.
+     * 
+     * @return the selected file, or null if no file was selected.
+     */
+    public static File getFileFromChooser() {
+        final File defaultDirectory = new File(
+                System.getProperty("user.dir") + System.getProperty("file.separator") + "games");
+        final JFileChooser chooser = new JFileChooser(defaultDirectory);
+
+        final FileNameExtensionFilter filter = new FileNameExtensionFilter("FEN files", FEN_EXTENSION);
+        chooser.setFileFilter(filter);
+        chooser.setDialogTitle("Load State");
+
+        final int returnState = chooser.showOpenDialog(null);
+        if (returnState == JFileChooser.APPROVE_OPTION) {
+            final File file = chooser.getSelectedFile();
+            return file;
+        }
+
+        return null;
+    }
+
+    /**
+     * Shuffles a string using the Fisher-Yates algorithm. This is used internally
+     * by the parser to generate a Fischer random position.
+     * 
+     * @param string The string to shuffle.
+     * @return The shuffled string.
+     */
+    private static String shuffleString(String string) {
+        final StringBuilder builder = new StringBuilder(string.length());
+        final List<Character> chars = new ArrayList<Character>();
+        for (final char c : string.toCharArray()) {
+            chars.add(c);
+        }
+        while (chars.size() != 0) {
+            final int randIndex = (int) (Math.random() * chars.size());
+            builder.append(chars.remove(randIndex));
+        }
+        return builder.toString();
+    }
+
+    /**
+     * Returns whether or not the first part of a FENString (the board-part) is
+     * legal for a Fischer random position.
+     * 
+     * @param string The FENString to check.
+     * @return Whether or not the first part of the FENString is legal for a Fischer
+     */
+    private static boolean legalFirstRowFischerRandom(String string) {
+        final int king = string.indexOf("k");
+        final int bishop1 = string.indexOf("b");
+        final int bishop2 = string.lastIndexOf("b");
+        final int rook1 = string.indexOf("r");
+        final int rook2 = string.lastIndexOf("r");
+
+        // Bishops must be on different colors. Same as that the sum of the two
+        // x-positions of the bishops must be an odd number.
+        if ((bishop1 + bishop2) % 2 != 1) {
+            return false;
+        }
+
+        // King must be between the two rooks
+        if (!(rook1 < king && king < rook2)) {
+            return false;
+        }
+
+        // Make sure we have the right count of every piece
+        final Map<Character, Integer> row = Map.of('r', 2, 'n', 2, 'b', 2, 'q', 1, 'k', 1);
+        for (final Character c : string.toCharArray()) {
+            if (string.chars().filter(e -> e == c).count() != row.get(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Get a valid {@link JFileChooser}, with correct settings already set, for
+     * saving a FEN file.
+     * 
+     * @param defaultSaveDirectory the directory to start the file chooser in.
+     * @return a valid {@link JFileChooser}.
+     */
     private static JFileChooser getValidChooser(File defaultSaveDirectory) {
-        JFileChooser chooser = new JFileChooser(defaultSaveDirectory) {
+        final JFileChooser chooser = new JFileChooser(defaultSaveDirectory) {
             // The following method is heavily inspired by stackoverflow:
             // https://stackoverflow.com/a/3729157/10880273
             @Override
             public void approveSelection() {
-                File file = getSelectedFile();
+                final File file = getSelectedFile();
                 if (file.exists() && getDialogType() == SAVE_DIALOG) {
-                    int result = JOptionPane.showConfirmDialog(this, "The file exists, overwrite?", "Existing file",
+                    final int result = JOptionPane.showConfirmDialog(this, "The file exists, overwrite?",
+                            "Existing file",
                             JOptionPane.YES_NO_CANCEL_OPTION);
                     switch (result) {
                         case JOptionPane.YES_OPTION:
@@ -293,23 +396,4 @@ public abstract class FENParser {
         };
         return chooser;
     }
-
-    public static File getFileFromChooser() {
-        File defaultDirectory = new File(
-                System.getProperty("user.dir") + System.getProperty("file.separator") + "games");
-        JFileChooser chooser = new JFileChooser(defaultDirectory);
-
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("FEN files", FEN_EXTENSION);
-        chooser.setFileFilter(filter);
-        chooser.setDialogTitle("Load State");
-
-        int returnState = chooser.showOpenDialog(null);
-        if (returnState == JFileChooser.APPROVE_OPTION) {
-            File file = chooser.getSelectedFile();
-            return file;
-        }
-
-        return null;
-    }
-
 }
